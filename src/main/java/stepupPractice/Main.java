d43898648bd9a01a5895b1e8ab4a2c065fc8d5ec
Sergei Kazakov <thaellyra@gmail.com>
2025-03-24 21:53:33 +0300

    Решение задачи "Практика ООП. Полиморфизм #1. Трехмерная точка"

--- src/main/java/stepupPractice/Main.java
+++ src/main/java/stepupPractice/Main.java
@@ -2,8 +2,9 @@ package stepupPractice;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Serega", new int[]{2, 3, 4, 5});
-        System.out.println(student1);
-        System.out.println(student1.getGrades());
+        Point3D point3D = new Point3D(1, 2, 3);
+        System.out.println(point3D.x);
+        System.out.println(point3D.y);
+        System.out.println(point3D.z);
     }
 }
---
+++ src/main/java/stepupPractice/Point.java
@@ -0,0 +1,10 @@
+package stepupPractice;
+
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
