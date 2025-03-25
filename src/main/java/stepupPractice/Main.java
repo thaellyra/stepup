b09a055ea15e5c318ad4792278c5373350feb2da
Sergei Kazakov <thaellyra@gmail.com>
2025-03-25 23:27:13 +0300

    Решение задачи "Практика ООП. * Полиморфизм #8. Птичий рынок - задание необязательное"

--- src/main/java/stepupPractice/Main.java
+++ src/main/java/stepupPractice/Main.java
@@ -2,16 +2,27 @@ package stepupPractice;
 
 public class Main {
     public static void main(String[] args) {
-        Vorobey vorobey = new Vorobey();
-        Kukushka kukushka = new Kukushka();
-        Popugay popugay = new Popugay("Расцветали яблони и груши");
+        Vorobey vorobey1 = new Vorobey();
+        Vorobey vorobey2 = new Vorobey();
+        Vorobey vorobey3 = new Vorobey();
+        Kukushka kukushka1 = new Kukushka();
+        Kukushka kukushka2 = new Kukushka();
+        Popugay popugay1 = new Popugay("Расцветали яблони и груши");
+        Popugay popugay2 = new Popugay("бим-бим бом-бом");
+        Popugay popugay3 = new Popugay("раз-два-три-четыре-пять");
 
-        sing(vorobey);
-        sing(kukushka);
-        sing(popugay);
+        singAll(vorobey1, vorobey2, vorobey3, kukushka1, kukushka2, popugay1, popugay2, popugay3);
     }
 
-    public static void sing(Singable s) {
-        s.sing();
+    public static void singAll(Bird... birds) {
+        int vorobeyCount = 1;
+        int kukushkaCount = 1;
+        int popugayCount = 1;
+        for (Bird bird : birds) {
+            if (bird instanceof Vorobey) System.out.println("==Воробей " + vorobeyCount++ + "==");
+            else if (bird instanceof Kukushka) System.out.println("==Кукушка " + kukushkaCount++ + "==");
+            else if (bird instanceof Popugay) System.out.println("==Попугай " + popugayCount++ + "==");
+            bird.sing();
+        }
     }
 }
