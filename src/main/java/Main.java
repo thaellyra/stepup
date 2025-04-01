756caf578c7627f6229324e59e4af444e776e200
Sergei Kazakov <thaellyra@gmail.com>
2025-04-02 01:49:38 +0300

    Решение задачи "Задача #2. Замена ArrayList на массив"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,50 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> intList = new ArrayList<>();
+        ArrayList<Integer> intList1 = new ArrayList<>();
         int n = 20;
 
         for (int i = 1; i <= n; i++) {
-            intList.add(i);
+            intList1.add((int) (Math.random() * 100));
         }
-        System.out.println(intList);
+        System.out.println(intList1);
+        bubbleSort(intList1);
+        System.out.println(intList1);
 
-        int tmp = 0;
-        for (int i = 0; i < intList.size(); i++) {
-            if (i % 2 == 1) {
-                intList.set(i - 1, intList.get(i));
-                intList.set(i, tmp);
-            } else {
-                tmp = intList.get(i);
+        System.out.println("====================");
+
+        int[] intList2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            intList2[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(intList2));
+        bubbleSort(intList2);
+        System.out.println(Arrays.toString(intList2));
+    }
+
+    public static void bubbleSort(ArrayList<Integer> intList) {
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
             }
         }
-        System.out.println(intList);
+    }
+
+    public static void bubbleSort(int[] intArray) {
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int j = 0; j < intArray.length - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int tmp = intArray[j];
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = tmp;
+                }
+            }
+        }
     }
 }
